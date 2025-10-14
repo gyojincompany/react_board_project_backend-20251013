2fb7d1ce7ba9616ebf62f3071e98468639f4a444
gyojincompany <luckyred@gmail.com>
2025-10-14 17:42:22 +0900

    글 수정 putmapping 매핑 추가

--- src/main/java/com/gyojincompany/home/controller/BoardController.java
+++ src/main/java/com/gyojincompany/home/controller/BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,24 +73,54 @@ public class BoardController {
 		
 	}
 	
-	//특정 id 글 삭제
+	//특정 id 글 삭제(삭제권한->로그인한 후 본인 글만 삭제 가능)
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deletePost(@PathVariable("id") Long id, Authentication auth) {
 
 		Optional<Board> _board = boardRepository.findById(id);
 		
+		//삭제할 글의 존재 여부 확인
+		if (_board.isEmpty()) { //참이면 삭제할 글이 존재하지 않음			
+			return ResponseEntity.status(404).body("해당 게시글은 존재하지 않아 삭제 실패하였습니다.");
+		}
+		
+		//로그인한 유저의 삭제 권한 확인
 		if (auth == null || !auth.getName().equals(_board.get().getAuthor().getUsername())) {
 			return ResponseEntity.status(403).body("해당 글에 대한 삭제 권한이 없습니다.");
 		}
 		
-		if (_board.isPresent()) { //참이면 삭제할 글 가져오기 성공
-			boardRepository.delete(_board.get());
-			return ResponseEntity.ok("글 삭제 성공");
-		} else { //거짓이면 해당 글 조회 실패
-			return ResponseEntity.status(404).body("해당 게시글은 존재하지 않아 삭제 실패하였습니다.");
-		}
+		
+		boardRepository.delete(_board.get());
+		return ResponseEntity.ok("글 삭제 성공");
 		
 	} 
+	
+	//게시글 수정(권한 설정->로그인 후 본인 작성글만 수정 가능)
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updatePost(
+			@PathVariable("id") Long id, 
+			@RequestBody Board updateBoard, 
+			Authentication auth) {
+		
+		Optional<Board> _board = boardRepository.findById(id);
+		
+		if (_board.isEmpty()) { //참이면 수정할 글이 존재하지 않음
+			return ResponseEntity.status(404).body("해당 게시글이 존재하지 않습니다.");
+		}
+		
+		if (auth == null || !auth.getName().equals(_board.get().getAuthor().getUsername())) {
+			return ResponseEntity.status(403).body("해당 글에 대한 수정 권한이 없습니다.");
+		}
+		
+		Board oldPost = _board.get(); //기존 게시글
+		
+		oldPost.setTitle(updateBoard.getTitle()); //제목 수정
+		oldPost.setContent(updateBoard.getContent()); //내용 수정
+		
+		boardRepository.save(oldPost); //수정한 내용 저장
+		
+		return ResponseEntity.ok(oldPost); //수정된 내용이 저장된 글 객체 반환
+	}
 	
 
 }
